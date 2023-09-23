4f124879c6e83114dacedba7fc8d2c54c8cf3695
Kari Vigna <karivigna24@gmail.com>
2023-09-22 18:35:18 -0700

    first two tests passing

---
+++ PierresTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PierresTracker.Models;
+using System;
+
+namespace PierresTracker.Tests
+{
+    [TestClass]
+    public class OrderTests
+    {
+
+        [TestMethod]
+        public void OrderConstructor_CreatesInstanceOfOrder_Order()
+        {
+            Order newOrder = new Order("test");
+            Assert.AreEqual(typeof(Order), newOrder.GetType());
+        }
+    }
+}
---
+++ PierresTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using PierresTracker.Models;
+using System;
+
+namespace PierresTracker.Tests
+{
+    [TestClass]
+    public class VendorTests
+    {
+
+        [TestMethod]
+        public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+        {
+            Vendor newVendor = new Vendor("Suzie's Cafe");
+            Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+        }
+    }
+}
