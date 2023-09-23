cba9c4784c21c2b781c1834b2d43530027f63940
Kari Vigna <karivigna24@gmail.com>
2023-09-23 10:27:55 -0700

    4 tests passing, wip

--- PierresTracker.Tests/ModelTests/OrderTests.cs
+++ PierresTracker.Tests/ModelTests/OrderTests.cs
@@ -6,14 +6,39 @@ using System;
 namespace PierresTracker.Tests
 {
     [TestClass]
-    public class OrderTests
+    public class OrderTests : IDisposable
     {
+        public void Dispose()
+        {
+            Order.ClearAll();
+        }
 
         [TestMethod]
         public void OrderConstructor_CreatesInstanceOfOrder_Order()
         {
             Order newOrder = new Order("test");
             Assert.AreEqual(typeof(Order), newOrder.GetType());
+        }
+
+        [TestMethod]
+        public void GetId_OrdersInstantiateWithAnId_Int()
+        {
+            string description = "Cookies";
+            Order newOrder = new Order(description);
+            int result = newOrder.Id;
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void GetAll_ReturnsOrders_OrderList()
+        {
+            string description1 = "Cookies";
+            string description2 = "Bagels";
+            Order newOrder1 = new Order(description1);
+            Order newOrder2 = new Order(description2);
+            List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+            List<Order> result = Order.GetAll();
+            CollectionAssert.AreEqual(newList, result);
         }
     }
 }
